dd2217852f79d8b0a582468f9aa7c3aae2c652ac
Sibayu <irmalove999@gmail.com>
2026-02-06 15:08:24 +0700

    backup getplugin.js

---
+++ plugins/owner/getplugin.js
@@ -0,0 +1,48 @@
+/** @type {import('#lib/types.js').Plugin} */
+import fs from 'fs'
+import path from 'path'
+import { sendFQ } from '#lib/fakeQuoted.js'
+
+export default {
+  name: "getplugin",
+  category: "owner",
+  command: ["gp", "getplugin"],
+
+  settings: {
+    owner: true,
+    private: false,
+    group: false,
+    admin: false,
+    botAdmin: false,
+    loading: false
+  },
+
+  run: async (conn, m) => {
+    if (!m.text) return sendFQ(conn, m, "Contoh: .gp tools/ping")
+
+    const input = m.text.trim()
+    const baseDir = path.join(process.cwd(), 'plugins')
+
+    let filePath
+
+    if (input.includes('/')) {
+      filePath = path.join(baseDir, input + '.js')
+    } else {
+      const folders = fs.readdirSync(baseDir)
+      for (const folder of folders) {
+        const possible = path.join(baseDir, folder, input + '.js')
+        if (fs.existsSync(possible)) {
+          filePath = possible
+          break
+        }
+      }
+    }
+
+    if (!filePath || !fs.existsSync(filePath)) {
+      return sendFQ(conn, m, "❌ Plugin tidak ditemukan.")
+    }
+
+    const code = fs.readFileSync(filePath, 'utf8')
+    sendFQ(conn, m, code)
+  }
+}
