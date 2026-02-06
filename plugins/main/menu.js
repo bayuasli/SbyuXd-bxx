200fde79facb742b270eebd94ab92b5fc81a780c
Sibayu <irmalove999@gmail.com>
2026-02-06 15:08:18 +0700

    backup menu.js

---
+++ plugins/main/menu.js
@@ -0,0 +1,58 @@
+export default {
+  name: "menu",
+  category: "main",
+  command: ["menu"],
+  run: async (conn, m, { Func }) => {
+
+    let grouped = {}
+    for (let plugin of Object.values(plugins)) {
+      if (!grouped[plugin.category]) grouped[plugin.category] = []
+      grouped[plugin.category].push(plugin)
+    }
+
+    const totalFitur = Object.values(grouped).flat().length
+    const totalKategori = Object.keys(grouped).length
+
+    const uptime = Func.runtime(process.uptime())
+    const ram = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(1)
+    const cpu = process.cpuUsage().user / 1000
+    const speed = (performance.now() - m.timesTamp).toFixed(2)
+
+    const time = new Date().toLocaleString("id-ID", {
+      timeZone: 'Asia/Jakarta',
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit"
+    })
+
+    let menu = `
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+┃ 𝗠𝗘𝗡𝗨 𝗦𝗜𝗕𝗔𝗬𝗨𝗫𝗗-𝗕𝗢𝗧
+┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+┃ 👤 User   : ${m.pushname}
+┃ 🌐 Mode   : ${IS_PUBLIC ? "PUBLIC" : "SELF"}
+┃ ⚡ Speed  : ${speed} ms
+┃ 🧠 RAM    : ${ram} MB
+┃ 🔧 CPU    : ${cpu} µs
+┃ ⏳ Uptime : ${uptime}
+┃ 🕒 Time   : ${time}
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+`
+
+    for (let [category, items] of Object.entries(grouped)) {
+      menu += `\n【 ${category.toUpperCase()} 】`
+      menu += `\n${items.map(p => `➤ ${m.prefix}${p.name}`).join("\n")}\n`
+    }
+
+    menu += `
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+┃ 📦 Categories : ${totalKategori}
+┃ 🧩 Features   : ${totalFitur}
+┃ 🗄 Database   : SQLite Engine
+┃ ⚙ Baileys    : baileys 7.0.0-rc.6
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+`
+
+    m.reply(menu)
+  }
+}
